dce43d9fd4002f25854f2f5b4b459c050e0879e7
mikhail.dementev@akvelon.com <mikhail.dementev@akvelon.com>
2015-07-21 16:24:19 +0300

    Minor changes.

--- src/test/java/skeleton/GoogleSearchSteps.java
+++ src/test/java/skeleton/GoogleSearchSteps.java
@@ -11,11 +11,11 @@ import org.openqa.selenium.By;
 public class GoogleSearchSteps {
     @Given("^I navigate to \"([^\"]*)\"$")
     public void I_navigate_to(String arg1) throws Throwable {
-        DriverHelper.getCurrentDriver().navigate().to(arg1);
+        DriverHelper.getCurrentDriver().navigate().to(java.lang.String.valueOf(arg1));
     }
 
     @When("^I search for \"([^\"]*)\"$")
-    public void I_search_for(String arg1) throws Throwable {
+    public void I_search_for(java.lang.String arg1) throws Throwable {
         DriverHelper.getCurrentDriver().findElement(By.id("lst-ib")).sendKeys(arg1);
     }
 
@@ -23,7 +23,7 @@ public class GoogleSearchSteps {
     public void I_found(String arg1) throws Throwable {
         Assert.assertEquals(arg1,
                 DriverHelper.getCurrentDriver().
-                        findElement(By.xpath("//*[@id=\"rso\"]/div[3]/li[1]/div/h3/a")).getText());
+                        findElement(By.xpath("//ol[@id='rso']/div[@class='srg']")).findElement(By.xpath("//li[@class='g']//h3[@class='r']/a[text()='" + arg1 + "']")).getText());
     }
 
     @And("^Title is \"([^\"]*)\"$")
